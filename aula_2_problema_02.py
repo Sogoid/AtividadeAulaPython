1a7f4de44015345ce15e50535f49ab916c54d69b
Sogoid <diogos_ribeiro@outlook.com>
2023-02-17 17:39:23 -0300

    Atualizações

---
+++ aula_2_problema_02.py
@@ -0,0 +1,52 @@
+"""finalidade: Calculo do IMC
+autor: Diogo da Silveia Ribeiro
+data: 14/02/2023
+Python versão: 3.9.13
+Link do repositorio no GitHub para analisar o código
+ -> https://github.com/Sogoid/AtividadeAula2
+"""
+
+print("\n** Programa de cadastro de cliente do Comércio Eletrônico **")
+print("******** Informe os dados para cadastro do cliente *********\n")
+
+nome = input("Nome: ")
+
+apelido = input("Apelido: ")
+
+print("Gênero:")
+genero = input("Masculino (M) / Feminino (F): ")
+
+telefone = int(input("Telefone: "))
+
+email = input("Email: ")
+
+endereco = input("Endereço: ")
+
+numero = int(input("Nº: "))
+
+barrio = input("Barrio: ")
+
+cidade = input("Cidade: ")
+
+estado = input("Estado: ")
+
+cep = input("CEP: ")
+
+senhas = input("Digita uma senha: ")
+
+rep_senhas = input("Digita sua senaha novamente: ")
+
+while senhas != rep_senhas:
+    print("\nSenhas diferentes. Tente novamente.\n")
+    senhas = input("Digita uma senha: ")
+
+    rep_senhas = input("Digita sua senaha novamente: ")
+else:
+    print("Senha cadastrada com sucesso!\n")
+
+
+print("*************** Relátorio do cadastro do cliente ***********\n")
+
+print(f"Nome: {nome}\nApelido: {apelido}\nGênero: {genero}\nTelefone: {telefone}\nE-mail: {email}")
+print(f"Endereço: {endereco}\nNº: {numero}\nBarrio: {barrio}\nCidade: {cidade}\nEstado: {estado}")
+print(f"CEP: {cep}\nSenha: {senhas}")
--- aula_2_problema_03.py
+++ aula_2_problema_03.py
@@ -1,7 +1,10 @@
-"""finalidade: Calculo do Salário
-autor: Diogo
+"""finalidade: Calculo do IMC
+autor: Diogo da Silveia Ribeiro
 data: 14/02/2023
 Python versão: 3.9.13
+Link do repositorio no GitHub para analisar o código
+ -> https://github.com/Sogoid/AtividadeAula2
 """
+
 print("\n****** Programa para Calculo do Salário Funcionário ******")
 print("*********** Informe os dados do funcionário **************\n")
