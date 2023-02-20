16988f5a8a093c05f6e634eda37cd59f2d94814a
Sogoid <diogos_ribeiro@outlook.com>
2023-02-19 23:40:15 -0300

    Atualizações

--- aula_2_problema_03.py
+++ aula_2_problema_03.py
@@ -50,23 +50,47 @@ insalubridade = salarioBase * insalubridade
 salario = (horasMensais * valorHora) + periculosidade + \
     insalubridade + transporte + alimentacao
 
+# Formatação dos valores na saídas.
+
+valorHora = f"{valorHora:_.2f}"
+valorHora = valorHora.replace(".", ",").replace("_", ".")
+
+transporte = f"{transporte:_.2f}"
+transporte = transporte.replace(".", ",").replace("_", ".")
+
+alimentacao = f"{alimentacao:_.2f}"
+alimentacao = alimentacao.replace(".", ",").replace("_", ".")
+
+periculosidade = f"{periculosidade:_.2f}"
+periculosidade = periculosidade.replace(".", ",").replace("_", ".")
+
+insalubridade = f"{insalubridade:_.2f}"
+insalubridade = insalubridade.replace(".", ",").replace("_", ".")
+
+salario = f"{salario:_.2f}"
+salario = salario.replace(".", ",").replace("_", ".")
+
+salarioBase = f"{salarioBase:_.2f}"
+salarioBase = salarioBase.replace(".", ",").replace("_", ".")
+
 print("********* Contra-Cheque dos dados do funcionário ***********\n")
 
-print(f"Nome: {nome}\n"
-      f"CPF: {cpf}"
+print(f"Nome: {nome.upper()}\n"
+      f"CPF:  {cpf}\n"
+      "----------------------------------------\n"
       )
 
-print(f"Valor da Hora  => R$ {valorHora:.2f}\n"
-      f"Transporte     => R$ {transporte:.2f}\n"
-      f"Alimentação    => R$ {alimentacao:.2f}\n"
-      f"Periculosidade => R$ {periculosidade:.2f}\n"
-      f"Insalubridade  => R$ {insalubridade:.2f}\n"
-      f"Salário        => R$ {salario:.2f}\n"
+print(f"Valor da Hora  => R$ {valorHora:_>10}\n"
+      f"Transporte     => R$ {transporte:_>10}\n"
+      f"Alimentação    => R$ {alimentacao:_>10}\n"
+      f"Periculosidade => R$ {periculosidade:_>10}\n"
+      f"Insalubridade  => R$ {insalubridade:_>10}\n"
+      f"Salário        => R$ {salario:_>10}\n"
       )
 
 print("******** Informações adicionais do Contra-Cheque **********\n")
 
 print(
-    f"Salário Base  => R$ {salarioBase:.2f}\n"
+    f"Salário Base  => R$ {salarioBase:_>10}\n"
     f"Horas Mensais => {horasMensais} HS"
 )
