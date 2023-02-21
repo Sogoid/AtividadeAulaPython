565f2925358834fe24ab07d3d81b8f1d3e070ae4
Sogoid <diogos_ribeiro@outlook.com>
2023-02-20 21:29:23 -0300

    Atualização e finalização

--- aula_2_problema_05.py
+++ aula_2_problema_05.py
@@ -11,25 +11,43 @@ print("\n********* Sistema para Cálculo de Combustível. ************\n"
       "*********** Informe os dados para o Cálculo. **************\n"
       )
 
+velocidade = float(input("Qual é a velocidade? => "))
+
+tempo = float(input("Quanto tempo de viagem? => "))
+
+litros = float(input("Quantos litros gastou de combustível? "))
+
 valorLitro = float(input("Preço do litro => R$ "))
 
-km = float(input("Quantos KM seu carro roda com 1 Litro? => "))
-
-velocidadeMedia = float(input("Qual é a velocidade média? => "))
-
-tempoGasto = float(input("Quanto tempo de viagem? => "))
-
-quantidadeLitros = float(input("Quantos litros foi gasto? => "))
-
-kmLitro = valorLitro / km
+# Calculo
 
 distancia = tempo * velocidade
 
+rendimento = distancia / litros
+
 litrosUsados = distancia / rendimento
 
-print(f"Velocidade média é                => {velocidadeMedia}"
-      f"Tempo gasto foi de                => {tempoGasto}"
-      f"Distancia percorrida foi de       => {distancia}"
-      f"Quantidade de litros gasto foi de => {litrosUsados}"
-      # f"Total do valor gasto foi de       =>{totalGasto}"
+totalGasto = litros * valorLitro
+
+# Formatação dos valores na saídas.
+
+tempo = f"{tempo:_.2f}"
+tempo = tempo.replace(".", ":").replace("_", ".")
+
+rendimento = f"{rendimento:_.2f}"
+rendimento = rendimento.replace(".", ",").replace("_", ".")
+
+litrosUsados = f"{litrosUsados:_.2f}"
+litrosUsados = litrosUsados.replace(".", ",").replace("_", ".")
+
+totalGasto = f"{totalGasto:_.2f}"
+totalGasto = totalGasto.replace(".", ",").replace("_", ".")
+
+print("\n********** Relatório de gastos na viagem. **************\n\n"
+      f"Velocidade média é                =>   {velocidade:>6}h/Km\n"
+      f"Tempo gasto foi de                =>   {tempo:>6}hs\n"
+      f"Distancia percorrida foi de       =>   {distancia:.2f}km\n"
+      f"Rendamento de combustível por km  =>   {rendimento:>6}Lt/Km\n"
+      f"Quantidade de litros gasto foi de =>   {litrosUsados:>6}LT\n"
+      f"Total do valor gasto foi de       =>R$ {totalGasto}"
       )
