8d92b62e58b3f08df1f4e8fced21bac598fc03e3
Sogoid <diogos_ribeiro@outlook.com>
2023-02-28 20:58:57 -0300

    Atualizações

--- aula_02_problema_07.py
+++ aula_02_problema_07.py
@@ -1,4 +1,4 @@
-"""Finalidade: Sistema de Ajuste de Preços.
+"""Finalidade: Sistema Locadora de Veículos.
 Autor: Diogo da Silveira Ribeiro
 data: 14/02/2023
 Versão: 0.1
@@ -7,13 +7,19 @@ Link do repositório no GitHub para analisar o código
  -> https://github.com/Sogoid/AtividadeAula2
 """
 
+<<<<<<< HEAD
 print("\n********** Sistema para Locadora de Veículos. *************\n"
       "*********** Informe os dados para o Cálculo. **************\n"
       )
+=======
+print(
+    "\n********* Sistema para Locadora de Veículos. ************\n"
+    "*********** Informe os dados para o Cálculo. **************\n"
+)
+>>>>>>> 27be0f8 (Atualizações)
 
 # Entrada de dados.
-valorAluguel = float(
-    input("Qual é o valor do aluguel do veículo por dia? => R$ "))
+valorAluguel = float(input("Qual é o valor do aluguel do veículo por dia? => R$ "))
 
 quantidadeVeiculo = int(input("Quantidade de Veículos da frota é => "))
 
@@ -57,8 +63,7 @@ faturamentoAnual = f"{faturamentoAnual:_.2f}"
 faturamentoAnual = faturamentoAnual.replace(".", ",").replace("_", ".")
 
 faturamentoTercoAnual = f"{faturamentoTercoAnual:_.2f}"
-faturamentoTercoAnual = faturamentoTercoAnual.replace(
-    ".", ",").replace("_", ".")
+faturamentoTercoAnual = faturamentoTercoAnual.replace(".", ",").replace("_", ".")
 
 atrasaDevolucao = f"{atrasaDevolucao:_.2f}"
 atrasaDevolucao = atrasaDevolucao.replace(".", ",").replace("_", ".")
@@ -66,6 +71,7 @@ atrasaDevolucao = atrasaDevolucao.replace(".", ",").replace("_", ".")
 atrasaDevolucoes = f"{atrasaDevolucoes:_.2f}"
 atrasaDevolucoes = atrasaDevolucoes.replace(".", ",").replace("_", ".")
 
+<<<<<<< HEAD
 print("\n********** Relatório do faturamento por mês. **************\n\n"
       f"Faturamento mensal Previsto é   => R${faturamento:>13}\n"
       f"Frota já alugada no mês é       => R${faturamentoTerco:>13}\n"
@@ -79,3 +85,19 @@ print("\n********** Relatório do faturamento por mês. **************\n\n"
       f"Quantidade de veículos comprados é       => {veiculoCompra:>2}\n"
       f"Total da Frota no final do Ano é         => {frota:>2}\n"
       )
+=======
+print(
+    "\n********** Relatório do faturamento por mês. **************\n\n"
+    f"Faturamento mensal Previsto é   => R${faturamento:>10}\n"
+    f"Frota já alugada no mês é       => R${faturamentoTerco:>10}\n"
+    f"Multa por atraso no mês         => R${atrasaDevolucoes:>10}\n"
+    "\n\n********** Relatório do faturamento por Ano. **************\n\n"
+    f"Faturamento Anual Previsto é    => R${faturamentoAnual:>10}\n"
+    f"Faturamento de um terço Anual é => R${faturamentoTercoAnual:>10}\n"
+    f"Multa por atraso no Anuais      => R${atrasaDevolucao:>10}\n"
+    "\n\n********** Relatório do estado da frota. **************\n\n"
+    f"Quantidade de veículos danificado no ano => {danificados:>15}\n"
+    f"Quantidade de veículos comprados é       => {veiculoCompra:>15}\n"
+    f"Total da Frota no final do Ano é         => {frota:>15}\n"
+)
+>>>>>>> 27be0f8 (Atualizações)
