9389adca6ba285b1ca97d930be7fb3c897778d32
Sogoid <diogos_ribeiro@outlook.com>
2023-02-28 21:08:23 -0300

    Update

--- aula_02_problema_07.py
+++ aula_02_problema_07.py
@@ -7,9 +7,10 @@ Link do repositório no GitHub para analisar o código
  -> https://github.com/Sogoid/AtividadeAula2
 """
 
-print("\n********** Sistema para Locadora de Veículos. *************\n"
-      "*********** Informe os dados para o Cálculo. **************\n"
-      )
+print(
+    "\n********** Sistema para Locadora de Veículos. *************\n"
+    "*********** Informe os dados para o Cálculo. **************\n"
+)
 
 # Entrada de dados.
 valorAluguel = float(input("Qual é o valor do aluguel do veículo por dia? => R$ "))
@@ -64,33 +65,17 @@ atrasaDevolucao = atrasaDevolucao.replace(".", ",").replace("_", ".")
 atrasaDevolucoes = f"{atrasaDevolucoes:_.2f}"
 atrasaDevolucoes = atrasaDevolucoes.replace(".", ",").replace("_", ".")
 
-<<<<<<< HEAD
-print("\n********** Relatório do faturamento por mês. **************\n\n"
-      f"Faturamento mensal Previsto é   => R${faturamento:>13}\n"
-      f"Frota já alugada no mês é       => R${faturamentoTerco:>13}\n"
-      f"Multa por atraso no mês         => R${atrasaDevolucoes:>13}\n"
-      "\n\n********** Relatório do faturamento por Ano. **************\n\n"
-      f"Faturamento Anual Previsto é    => R${faturamentoAnual:>13}\n"
-      f"Faturamento de um terço Anual é => R${faturamentoTercoAnual:>13}\n"
-      f"Multa por atraso no Anuais      => R${atrasaDevolucao:>13}\n"
-      "\n\n************ Relatório do estado da frota. ****************\n\n"
-      f"Quantidade de veículos danificado no ano => {danificados:>2}\n"
-      f"Quantidade de veículos comprados é       => {veiculoCompra:>2}\n"
-      f"Total da Frota no final do Ano é         => {frota:>2}\n"
-      )
-=======
 print(
     "\n********** Relatório do faturamento por mês. **************\n\n"
-    f"Faturamento mensal Previsto é   => R${faturamento:>10}\n"
-    f"Frota já alugada no mês é       => R${faturamentoTerco:>10}\n"
-    f"Multa por atraso no mês         => R${atrasaDevolucoes:>10}\n"
+    f"Faturamento mensal Previsto é   => R${faturamento:>13}\n"
+    f"Frota já alugada no mês é       => R${faturamentoTerco:>13}\n"
+    f"Multa por atraso no mês         => R${atrasaDevolucoes:>13}\n"
     "\n\n********** Relatório do faturamento por Ano. **************\n\n"
-    f"Faturamento Anual Previsto é    => R${faturamentoAnual:>10}\n"
-    f"Faturamento de um terço Anual é => R${faturamentoTercoAnual:>10}\n"
-    f"Multa por atraso no Anuais      => R${atrasaDevolucao:>10}\n"
-    "\n\n********** Relatório do estado da frota. **************\n\n"
-    f"Quantidade de veículos danificado no ano => {danificados:>15}\n"
-    f"Quantidade de veículos comprados é       => {veiculoCompra:>15}\n"
-    f"Total da Frota no final do Ano é         => {frota:>15}\n"
+    f"Faturamento Anual Previsto é    => R${faturamentoAnual:>13}\n"
+    f"Faturamento de um terço Anual é => R${faturamentoTercoAnual:>13}\n"
+    f"Multa por atraso no Anuais      => R${atrasaDevolucao:>13}\n"
+    "\n\n************ Relatório do estado da frota. ****************\n\n"
+    f"Quantidade de veículos danificado no ano => {danificados:>2}\n"
+    f"Quantidade de veículos comprados é       => {veiculoCompra:>2}\n"
+    f"Total da Frota no final do Ano é         => {frota:>2}\n"
 )
->>>>>>> 27be0f8 (Atualizações)
