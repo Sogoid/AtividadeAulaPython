48dced791f444be3032f8b1cc7b48a655b4d7ad3
Sogoid <diogos_ribeiro@outlook.com>
2023-03-14 00:18:19 -0300

    File activity 09

---
+++ AtividadeAula04/aula_04_problema_09.py
@@ -0,0 +1,13 @@
+"""Finalidade: Cálculo de Notas dos Alunos de Umas Instituição de Ensino
+autor: Diogo da Silveira Ribeiro
+data: 13/03/2023
+Versão: 0.1
+Python versão: 3.10.9
+Link do repositório no GitHub para analisar o código
+ -> https://github.com/Sogoid/AtividadeAulaPython
+"""
+
+print(
+    "\n*********** Sistema de notas escolares. *************\n"
+    "*********** Informe as notas do Aluno. **************\n"
+)
