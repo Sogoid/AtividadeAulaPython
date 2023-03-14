36e88b3daa50758b2f4a384cf384c3c7793f59b9
Sogoid <diogos_ribeiro@outlook.com>
2023-03-14 17:42:56 -0300

    End activity problem 09

--- AtividadeAula04/aula_04_problema_09.py
+++ AtividadeAula04/aula_04_problema_09.py
@@ -1,4 +1,4 @@
-"""Finalidade: Cálculo de Notas dos Alunos de Umas Instituição de Ensino
+"""Finalidade: Para identificar diferença entre número
 autor: Diogo da Silveira Ribeiro
 data: 13/03/2023
 Versão: 0.1
@@ -6,8 +6,43 @@ Python versão: 3.10.9
 Link do repositório no GitHub para analisar o código
  -> https://github.com/Sogoid/AtividadeAulaPython
 """
+import random
 
 print(
-    "\n*********** Sistema de notas escolares. *************\n"
-    "*********** Informe as notas do Aluno. **************\n"
+    "\n*********** Sistema para identificar diferença entre número. *************\n"
+    "********************** Informe as número aleatório. **********************\n"
 )
+
+x = round(random.randrange(1, 100 + 1))
+print(f"Digite um número para X => {x} ")
+y = round(random.randrange(1, 100 + 1))
+print(f"Digite um número para Y => {y} ")
+z = round(random.randrange(1, 100 + 1))
+print(f"Digite um número para Z => {z}")
+
+print("\n*********** Qual é o maior número? *************\n")
+
+if x > y and x > z:
+    print(f"O maior número é X => {x}")
+elif y > x and y > z:
+    print(f"O maior número é Y => {y}")
+elif z > x and x > y:
+    print(f"O maior número é Z => {z}")
+
+print("\n*********** Qual é o menor número? *************\n")
+
+if x < y and x < z:
+    print(f"O menor número é X => {x}")
+elif y < x and y < z:
+    print(f"O menor número é Y => {y}")
+elif z < x and x < y:
+    print(f"O menor número é Z => {z}")
+
+print("\n*********** Qual é número do meio? *************\n")
+
+if x > y and x < z:
+    print(f"O número do meio é X => {x}\n")
+elif y < x and y > z:
+    print(f"O número do meio é Y => {y}\n")
+elif z > x and x < y:
+    print(f"O número do meio é Z => {z}\n")
