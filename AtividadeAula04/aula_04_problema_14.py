9f8252bf56e3ea14a7cd25a93834e3503f604ee2
Sogoid <diogos_ribeiro@outlook.com>
2023-03-16 17:45:59 -0300

    update

--- AtividadeAula04/aula_04_problema_14.py
+++ AtividadeAula04/aula_04_problema_14.py
@@ -1,7 +1,7 @@
 """Finalidade: Verificação se o número e impar ou par
 autor: Diogo da Silveira Ribeiro
 data: 13/03/2023
-Versão: 0.1
+Versão: 0.2
 Python versão: 3.10.9
 Link do repositório no GitHub para analisar o código
  -> https://github.com/Sogoid/AtividadeAulaPython
@@ -13,12 +13,15 @@ print(
     "******* Informe as número aleatório. ********\n"
 )
 
-numero = round(random.randrange(1, 20 + 1))
-print(f"Digite um número => {numero}\n")
+nuns = []
 
-numero = numero % 2
+while len(nuns) <= 10:
 
-if numero == 0:
+    numero = round(random.randrange(1, 20 + 1))
+    print(f"Digite um número => {numero}\n")
+
+if numero % 2 == 0:
+    nuns.append(numero) 
     print("O número é par\n")
 
 else:
