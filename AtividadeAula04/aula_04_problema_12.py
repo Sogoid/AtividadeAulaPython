9aedf07d6b155d9f5b423b921a86472aa9ca2c60
Sogoid <diogos_ribeiro@outlook.com>
2023-03-16 15:36:46 -0300

    Complete problem file 12

--- AtividadeAula04/aula_04_problema_12.py
+++ AtividadeAula04/aula_04_problema_12.py
@@ -6,43 +6,47 @@ Python versão: 3.10.9
 Link do repositório no GitHub para analisar o código
  -> https://github.com/Sogoid/AtividadeAulaPython
 """
-import random
 
 print(
     "\n** Sistema para verificação de triângulo. **\n"
     "******* Informe as número aleatório. ********\n"
 )
 
-l1 = round(random.randrange(1, 10 + 1))
-print(f"Digite o lado 1 => {l1}")
+# Utilize os valores de 3,3,3 para Triângulo Equilátero
+# Utilize os valores de 8,9,10 para Triângulo Escaleno
+# Utilize os valores de 5,6,5 para Triângulo Isósceles
+# Utilize os valores de 3,4,5 para Triângulo Retângulo
 
-l2 = round(random.randrange(1, 10 + 1))
-print(f"Digite o lado 2 => {l2}")
+l1 = int(input("Digite lado 1 => "))
 
-l3 = round(random.randrange(1, 10 + 1))
-print(f"Digite o lado 3 => {l3}")
+l2 = int(input("Digite lado 2 => "))
+
+l3 = int(input("Digite lado 3 => "))
 
 
 print("\n******* Retorno da verificação do triângulo ********\n")
 
+# Verifica se os lados formam um triângulo
 if l1 < (l2 + l3) and l2 < (l1 + l3) and l3 < (l1 + l2):
-    print("Existe um triângulo\n")
+    # Se formam um triângulo, verifica o tipo
+    if l1 == l2 == l3:
+        print("Forma Triângulo equilátero\n")
 
-    if l1 == l2 and l1 == l3 and l2 == l3:
-        print("\nForma triângulo equilátero\n")
-
-    elif l1 == l2 or l1 == l3 or l2 == l3:
-        print("Forma triângulo isosceles\n")
+    elif ((l1 == l2 and l1 != l3)
+          or (l1 == l3 and l2 != l1) or (l2 == l3 and l3 != l1)):
+        print("\nForma Triângulo isósceles\n")
 
     elif (
-        (l1**2 == l2**2) + l3**2
-        or (l2**2 == l1**2) + l3**2
-        or (l3**2 == l2**2) + l1**2
+        l1**2 == (l2**2 + l3**2)
+        or l2**2 == (l1**2 + l3**2)
+        or l3**2 == (l2**2 + l1**2)
     ):
-        print("")
+        print("Forma Triângulo Retângulo")
 
-    elif l1 != l2 and l1 != l3 and l2 != l3:
-        print("Forma triângulo escaleno\n")
+    # verifica se os lados são diferentes entre si
+    elif l1 != l2 or l1 != l3 or l2 != l3:
+        print("Forma Triângulo escaleno\n")
+
 
 else:
-    print("Não existe um triângulo\n")
+    print("Não é possível formar um triângulo com essas medidas\n")
