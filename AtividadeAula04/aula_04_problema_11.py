fc29bbdf281888c7cbafe5cb458dcdb6daae6920
Sogoid <diogos_ribeiro@outlook.com>
2023-03-17 12:46:44 -0300

    Complete issue file problem 11, 15 , 16

--- AtividadeAula04/aula_04_problema_11.py
+++ AtividadeAula04/aula_04_problema_11.py
@@ -21,17 +21,17 @@ imc = peso / (altura**2)
 print("\n*************** Ficha médica do paciente **************\n")
 
 print(
-    f"Paciente: {nome}\n"
-    f"Idade: {idade}\n"
-    f"Peso: {peso}\n"
-    f"Altura: {altura}\n"
-)
+      f"Paciente: {nome}\n"
+      f"Idade: {idade}\n"
+      f"Peso: {peso}\n"
+      f"Altura: {altura}\n"
+    )
 
 print(f"Valor do IMC é: {imc:.2f}\n")
 
 print("*************** Resultado medico **********************\n")
 
-if 19.1 >= imc and imc <= 25.8:
+if imc >= 19.1 and imc <= 25.8:
     print(f"Paciente: {nome!r} esta com peso normal com {imc:.2f}.\n")
 
 elif imc >= 25.8 and imc <= 27.3:
---
+++ AtividadeAula04/aula_04_problema_15.py
@@ -0,0 +1,106 @@
+"""Finalidade: Cadastro de Funcionário de uma Empresa
+Autor: Diogo da Silveira Ribeiro
+data: 17/03/2023
+Versão: 0.2
+Python versão: 3.10.9
+Link do repositório no GitHub para analisar o código
+ -> https://github.com/Sogoid/AtividadeAula2
+"""
+
+print("\n****** Programa para Calculo do Salário Funcionário ******\n")
+__cont__ = 0
+
+while __cont__ <= 4:
+
+    __cont__ += 1
+
+    print(f"******* Informe os dados do funcionário {__cont__} ********\n")
+    # Dados do cadastro do funcionário.
+    nome = input("Nome: ")
+    cpf = int(input("CPF: "))
+
+    # Dados referente a calculo do salário.
+    salarioBase = float(input("Salario Base: "))
+    transporte = int(input("Valor do Vale Transporte: "))
+    alimentacao = int(input("Valor do Vale Alimentação: "))
+    periculosidade = float(input("Porcentagem da Periculosidade: "))
+    insalubridade = float(input("Porcentagem da Insalubridade: "))
+    jornada = 40
+    quantidadesemana = 4
+    quantidadedias = 5
+
+    print()
+
+    # Calculo para descobrir a quantidade de dias.
+    dias = quantidadesemana * quantidadedias
+
+    # Calculo do vale-transporte e Alimentação.
+    transporte = dias * transporte
+
+    alimentacao = dias * alimentacao
+
+    # Calculo Horas Trabalhadas.
+    horasMensais = jornada * quantidadesemana
+
+    valorHora = salarioBase / horasMensais
+
+    # Calculo da Pericolosidade e Insalubriade.
+    periculosidade = salarioBase * periculosidade
+
+    insalubridade = salarioBase * insalubridade
+
+    # Calculo do Salário.
+    salario = (
+        (horasMensais * valorHora)
+        + periculosidade
+        + insalubridade
+        + transporte
+        + alimentacao
+    )
+
+    # Formatação dos valores na saídas.
+
+    valorHora = f"{valorHora:_.2f}"
+    valorHora = valorHora.replace(".", ",").replace("_", ".")
+
+    transporte = f"{transporte:_.2f}"
+    transporte = transporte.replace(".", ",").replace("_", ".")
+
+    alimentacao = f"{alimentacao:_.2f}"
+    alimentacao = alimentacao.replace(".", ",").replace("_", ".")
+
+    periculosidade = f"{periculosidade:_.2f}"
+    periculosidade = periculosidade.replace(".", ",").replace("_", ".")
+
+    insalubridade = f"{insalubridade:_.2f}"
+    insalubridade = insalubridade.replace(".", ",").replace("_", ".")
+
+    salario = f"{salario:_.2f}"
+    salario = salario.replace(".", ",").replace("_", ".")
+
+    salarioBase = f"{salarioBase:_.2f}"
+    salarioBase = salarioBase.replace(".", ",").replace("_", ".")
+
+    print(f"*** Contra-Cheque dos dados do funcionário {__cont__} ***\n")
+
+    print(
+        f"Nome: {nome.upper()}\n"
+        f"CPF:  {cpf}\n"
+        "----------------------------------------\n"
+    )
+
+    print(
+        f"Valor da Hora  => R$ {valorHora:_>10}\n"
+        f"Transporte     => R$ {transporte:_>10}\n"
+        f"Alimentação    => R$ {alimentacao:_>10}\n"
+        f"Periculosidade => R$ {periculosidade:_>10}\n"
+        f"Insalubridade  => R$ {insalubridade:_>10}\n"
+        f"Salário        => R$ {salario:_>10}\n"
+    )
+
+    print(f"*** Informações adicionais do Contra-Cheque {__cont__} ***\n")
+
+    print(
+        f"Salário Base  => R$ {salarioBase:_>10}\n"
+        f"Horas Mensais => {horasMensais} HS\n"
+    )
