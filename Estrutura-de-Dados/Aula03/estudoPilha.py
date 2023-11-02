b0e5adec36fad0bce53026bc16f6ddec85bc7156
DIOGO DA S RIBEIRO <116321555+Sogoid@users.noreply.github.com>
2023-11-01 21:59:48 -0300

    Update estudoPilha.py

--- Estrutura-de-Dados/Aula03/estudoPilha.py
+++ Estrutura-de-Dados/Aula03/estudoPilha.py
@@ -1,5 +1,16 @@
 import numpy as np
 
 
-def teste():
-    return
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+    def mostra_no(self) -> object:
+        print(self.valor)
+
+
+# Testes
+
+no1 = No(10)
+no1.mostra_no()
